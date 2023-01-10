9d33fc30864d350fe843232513c957a8a1f8da50
Fadhel Ben Abdi <fadhel.abdi@DOLF.LOCAL>
2023-01-10 15:33:25 +0300

    map url to the next page

--- Django_Dolf/layground/views.py
+++ Django_Dolf/layground/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# get the resuest -> response
+
+
+# This is what I did in this
+
+def say_hello(request):
+    return HttpResponse("Hello world From Dolf...")
+
+
+
